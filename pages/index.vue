95574571372330e4cb8d0ad273e37117a5c1260c
윤수혁 <cooltime15@likelion.org>
2021-07-28 20:30:17 +0900

    first init project

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        {{ message }}
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+
+    @Component({})
+    export default class Index extends Vue {
+        message: string = '안녕하세요';
+    }
+</script>
