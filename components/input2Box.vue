764187b340bcfd8214ef0cfa4824239a4891fb0f
윤수혁 <cooltime15@likelion.org>
2021-07-29 12:20:56 +0900

    Add Button Component

---
+++ components/input2Box.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="box">
+        <button class="inbox" id="top" @click="modal">{{top.top}}</button>
+        <button class="inbox" id="bottom">{{top.bottom}}</button>
+    </div>
+</template>
+<style>
+.box {
+    display: inline-block;
+}
+.inbox {
+    display: block;
+}
+button {
+    border-radius: 25px;
+    margin-right: 5px;
+    cursor: pointer; 
+    outline: none; 
+    box-shadow: 1px 4px 0 rgb(0,0,0,0.5); 
+    }
+button:active {
+     box-shadow: 1px 1px 0 rgb(0,0,0,0.5); 
+     position: relative; 
+     top:2px; 
+    }
+</style>
+
+<script lang="ts">
+    import { Component, Prop, Vue, Emit } from 'vue-property-decorator';
+
+    interface card {
+        top: string;
+        bottom: string;
+    }
+
+    @Component
+    export default class Input2Box extends Vue {
+        @Prop() top?: card[];
+        dialog: Boolean = false;
+
+        @Emit()
+        chan(){
+            console.log("자식"+this.top)
+            return this.top;
+        }
+
+        modal(){
+            alert("hi");
+        }
+    }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,62 @@
 <template>
     <div>
-        {{ message }}
+        <div class="boxs" v-for="item in topArray" :key="item.id">
+            <input-2-box @chan="submit" :top="item"></input-2-box>
+        </div>
+        
+        <v-text-field
+            label="입력해주세요"
+            solo
+            @keyup.enter="submit"
+            v-model="message"
+        ></v-text-field>
     </div>
 </template>
-
+<style >
+ .boxs {
+     display: inline;
+ }
+ input-2-box {
+   display: block;  
+ }
+</style>
+</style>
 <script lang="ts">
-    import { Component, Vue } from 'vue-property-decorator';
+    import { Component, Vue, Watch } from 'vue-property-decorator';
+    import Input2Box from '@/components/input2Box.vue';
 
-    @Component({})
+    interface card {
+        top: string;
+        bottom: string;
+    }
+
+    @Component({
+        components: {Input2Box}
+    })
     export default class Index extends Vue {
-        message: string = '안녕하세요';
+        message: string = '';
+        topArray: card[] = [];
+
+        @Watch('topArray', {immediate: true, deep: true})
+        update(newValue:card[], oldValue:card[]){
+            console.log(newValue, oldValue);
+        }
+        
+        submit(){
+            const temp: string[]|undefined = this.message?.split(' ');
+            if(temp){ 
+                console.log(temp);
+                temp.forEach(
+                    i => {
+                        
+                        this.topArray.push( {
+                            top: i,
+                            bottom: "__".repeat(i.length)
+                        })
+                    }
+                );
+            }
+            this.message = '';
+        }
     }
 </script>
